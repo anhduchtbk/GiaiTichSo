b8a1b42fe8fb44a1656facc855f9535e075d91c5
AnhDuc <79646902+anhduchtbk@users.noreply.github.com>
2022-11-03 23:31:39 +0700

    Update test.py

--- Week5/test.py
+++ Week5/test.py
@@ -38,20 +38,20 @@ print(x)
 # print(linalg.inv(A))
 
 
-from time import time
+# from time import time
 
-def Newton(f, df, x0, tol):
-    x_old = x0
-    while(abs(f(x_old)) > tol and df(x_old) != 0):
-        x_new = x_old - f(x_old) / df(x_old)
-        x_old = x_new
-    return x_old
-f = lambda x: x - x**(1/3) - 2
-df = lambda x: 1 - 1/3 * x**(-2/3)
-x0 = 2.5
-tol = 1e-6
-tic = time()
-x = Newton(f, df, x0, tol)
-toc = time()
-print ( 'No su dung phuong phap Newton la:',x)
-print('Processing tiem is =', toc - tic)
+# def Newton(f, df, x0, tol):
+#     x_old = x0
+#     while(abs(f(x_old)) > tol and df(x_old) != 0):
+#         x_new = x_old - f(x_old) / df(x_old)
+#         x_old = x_new
+#     return x_old
+# f = lambda x: x - x**(1/3) - 2
+# df = lambda x: 1 - 1/3 * x**(-2/3)
+# x0 = 2.5
+# tol = 1e-6
+# tic = time()
+# x = Newton(f, df, x0, tol)
+# toc = time()
+# print ( 'No su dung phuong phap Newton la:',x)
+# print('Processing tiem is =', toc - tic)
